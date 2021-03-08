598d01e67c34b5f12221c8726e6e90a2d6b99c30
Ali Akbar Mohammadi <cyrusgp@gmail.com>
2021-03-08 22:33:56 +0800

    fix transactions amount

--- src/js/store.js
+++ src/js/store.js
@@ -4,7 +4,7 @@ import { createStore } from 'framework7';
 const store = createStore({
   state: {
     coins: null,
-    current_coin: { name: 'BTC', type: 0, is_in_discovey: false },
+    current_coin: { name: 'BTC', type: 0, is_in_discovey: false, decimals: 8 },
     device: null,
     device_features: null,
     wallets: {},
